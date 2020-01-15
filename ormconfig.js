7828d5c2251f7453ed4a7e877ae9133163ef47ea
tighug <kapuriko123@gmail.com>
2020-01-15 13:39:08 +0900

    [Add] TypeORMの設定ファイルを追加

---
+++ ormconfig.js
@@ -0,0 +1,13 @@
+const TypeOrmNamingStrategy = require("./config/TypeOrmNamingStrategy.js");
+
+module.exports = {
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "root",
+  password: "root",
+  database: "spm_db",
+  entities: ['dist/**/*.entity.js'],
+  synchronize: false,
+  namingStrategy: new TypeOrmNamingStrategy()
+};
