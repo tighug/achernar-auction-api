5a74a9420ecb33950d481d574dc98557c4b5589c
tighug <kapuriko123@gmail.com>
2020-01-15 23:27:56 +0900

    [Fix] MarketResultモデルの主キー宣言を自動生成主キー宣言に修正

--- src/market-results/market-result.entity.ts
+++ src/market-results/market-result.entity.ts
@@ -1,9 +1,9 @@
-import { Entity, PrimaryColumn, ManyToOne, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, ManyToOne, Column } from "typeorm";
 import { Market } from "src/markets/market.entity";
 
 @Entity()
 export class MarketResult {
-  @PrimaryColumn()
+  @PrimaryGeneratedColumn()
   id: number;
 
   @ManyToOne(() => Market)
