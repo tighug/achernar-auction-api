c4e7c380739743224cf1d995285d26ab686beb40
tighug <kapuriko123@gmail.com>
2020-01-16 22:41:01 +0900

    [Add] Auctionコントローラの機能拡充

--- src/auctions/auctions.controller.ts
+++ src/auctions/auctions.controller.ts
@@ -4,6 +4,8 @@ import { Queue } from "bull";
 import { NodesService } from "src/nodes/nodes.service";
 import { WiresService } from "src/wires/wires.service";
 import { MarketsService } from "src/markets/markets.service";
+import { BidsService } from "src/bids/bids.service";
+import { FlowTransitionsService } from "src/flow-transitions/flow-transitions.service";
 
 @Controller("auctions")
 export class AuctionsController {
@@ -11,7 +13,10 @@ export class AuctionsController {
     @InjectQueue("auction")
     private readonly _auctionQueue: Queue,
     private readonly _nodesService: NodesService,
-    private readonly _wiresService: WiresService
+    private readonly _wiresService: WiresService,
+    private readonly _marketsService: MarketsService,
+    private readonly _bidsService: BidsService,
+    private readonly _flowTransitionsService: FlowTransitionsService
   ) {}
 
   @Post("start")
@@ -21,12 +26,19 @@ export class AuctionsController {
     // console.log("posted: ", job.id);
   }
 
-  async startAuction(feederId: number): Promise<void> {
+  async startAuction(marketId: number): Promise<void> {
+    const market = await this._marketsService.findById(marketId);
+    const feederId = market.feeder.id;
     const [nodes, nodeCount] = await this._nodesService.findByFeederId(
       feederId
     );
     const [wires, wireCount] = await this._wiresService.findByFeederId(
       feederId
     );
+    const [bids, bidCount] = await this._bidsService.findByMarketId(marketId);
+    const [
+      flowTransitions,
+      flowTransitionCount
+    ] = await this._flowTransitionsService.findByMarketId(marketId);
   }
 }
