b3b4163a3b6dafa195bfe00ee778e85dfd16f5ba
tighug <kapuriko123@gmail.com>
2020-01-16 19:07:37 +0900

    [Add] Rootモジュールにdotenvのインポートを追加

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import * as dotenv from "dotenv";
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   await app.listen(3001);
 }
+
+dotenv.config();
 bootstrap();
