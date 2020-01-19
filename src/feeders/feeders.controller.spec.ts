0d316e7aaa4dbf450885ef1eb6d88a3c5ae6f5a9
tighug <kapuriko123@gmail.com>
2020-01-19 16:12:40 +0900

    [Upgade] FeederのControllernのテストを拡充

--- src/feeders/feeders.controller.spec.ts
+++ src/feeders/feeders.controller.spec.ts
@@ -1,18 +1,63 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { FeedersController } from "./feeders.controller";
+import { FeedersService } from "./feeders.service";
+import { Feeder } from "./feeder.entity";
+import { getRepositoryToken } from "@nestjs/typeorm";
 
 describe("Feeders Controller", () => {
   let controller: FeedersController;
+  let service: FeedersService;
+  let feeders: Feeder[];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [FeedersController]
+      controllers: [FeedersController],
+      providers: [
+        FeedersService,
+        {
+          provide: getRepositoryToken(Feeder),
+          useValue: null
+        }
+      ]
     }).compile();
 
     controller = module.get<FeedersController>(FeedersController);
+    service = module.get<FeedersService>(FeedersService);
+    feeders = [
+      {
+        id: 1,
+        networkNum: 1,
+        feederNum: 1,
+        nodeCount: 10,
+        houseCount: 100
+      },
+      {
+        id: 2,
+        networkNum: 1,
+        feederNum: 2,
+        nodeCount: 20,
+        houseCount: 200
+      }
+    ];
   });
 
   it("should be defined", () => {
     expect(controller).toBeDefined();
+  });
+
+  describe("findAll", () => {
+    it("OK", async () => {
+      const expectedResult = {
+        feeders: feeders,
+        feederCount: 2
+      };
+      jest
+        .spyOn(service, "findAll")
+        .mockImplementation(async () => [feeders, feeders.length]);
+
+      const result = await controller.findAll();
+
+      expect(result).toStrictEqual(expectedResult);
+    });
   });
 });
