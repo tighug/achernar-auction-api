5231e7847d6d83024ddca02fb8e86d7a9d28a445
tighug <kapuriko123@gmail.com>
2020-01-20 20:16:06 +0900

    [Upgrade] ValidationPipeを追加

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as dotenv from "dotenv";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: false
+    })
+  );
   await app.listen(3001);
 }
 
